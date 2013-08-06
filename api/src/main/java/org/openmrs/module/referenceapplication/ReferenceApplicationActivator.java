a0569ecb896c3433237fbf79f3e07d68aa9aef11
dkayiwa <dkayiwa@openmrs.org>
2013-08-06 15:56:29 +0300

    Configuring namephonetics on startup: Soundex searching for similar patient matching - RA-163

--- api/src/main/java/org/openmrs/module/referenceapplication/ReferenceApplicationActivator.java
+++ api/src/main/java/org/openmrs/module/referenceapplication/ReferenceApplicationActivator.java
@@ -23,6 +23,8 @@ import org.openmrs.module.ModuleActivator;
 import org.openmrs.module.appframework.service.AppFrameworkService;
 import org.openmrs.module.emrapi.EmrApiConstants;
 import org.openmrs.module.referencemetadata.ReferenceMetadataProperties;
+import org.openmrs.module.namephonetics.NamePhoneticsConstants;
+import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
@@ -65,6 +67,8 @@ public class ReferenceApplicationActivator extends BaseModuleActivator {
                 "registrationcore.ExistingPatientNameSearch"));
 
         setupEmrApiGlobalProperties(administrationService);
+        setupNamePhoneticsGlobalProperties(administrationService);
+        setupRegistrationcoreGlobalProperties(administrationService);
 
         log.info("Reference Application Module started");
 	}
@@ -78,6 +82,19 @@ public class ReferenceApplicationActivator extends BaseModuleActivator {
         setGlobalProperty(administrationService, EmrApiConstants.GP_AT_FACILITY_VISIT_TYPE, ReferenceMetadataProperties.FACILITY_VISIT_TYPE_UUID);
     }
 
+    private void setupNamePhoneticsGlobalProperties(AdministrationService administrationService) {
+		setGlobalProperty(administrationService, NamePhoneticsConstants.GIVEN_NAME_GLOBAL_PROPERTY, "Soundex");
+		setGlobalProperty(administrationService, NamePhoneticsConstants.MIDDLE_NAME_GLOBAL_PROPERTY, "Soundex");
+		setGlobalProperty(administrationService, NamePhoneticsConstants.FAMILY_NAME_GLOBAL_PROPERTY, "Soundex");
+		setGlobalProperty(administrationService, NamePhoneticsConstants.FAMILY_NAME2_GLOBAL_PROPERTY, "Soundex");
+	}
+	
+	private void setupRegistrationcoreGlobalProperties(AdministrationService administrationService) {
+		setGlobalProperty(administrationService, RegistrationCoreConstants.GP_PATIENT_NAME_SEARCH, "registrationcore.NamePhoneticsPatientNameSearch");
+		setGlobalProperty(administrationService, RegistrationCoreConstants.GP_FAST_SIMILAR_PATIENT_SEARCH_ALGORITHM, "registrationcore.NamePhoneticsPatientSearchAlgorithm");
+		setGlobalProperty(administrationService, RegistrationCoreConstants.GP_PRECISE_SIMILAR_PATIENT_SEARCH_ALGORITHM, "registrationcore.NamePhoneticsPatientSearchAlgorithm");
+	}
+	
     private void setGlobalProperty(AdministrationService administrationService, String propertyName, String propertyValue) {
         GlobalProperty gp = administrationService.getGlobalPropertyObject(propertyName);
         if (gp == null) {
