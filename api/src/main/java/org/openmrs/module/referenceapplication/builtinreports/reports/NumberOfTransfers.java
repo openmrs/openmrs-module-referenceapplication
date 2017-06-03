0eba8c39a05956ba44bdc3c31f598e009c57d48b
JudeNiroshan <jude.niroshan11@gmail.com>
2017-06-03 16:52:27 +0530

    initial report bunch(6 reports) which implemented with SQL DataDefinitions.

---
+++ api/src/main/java/org/openmrs/module/referenceapplication/builtinreports/reports/NumberOfTransfers.java
@@ -0,0 +1,80 @@
+package org.openmrs.module.referenceapplication.builtinreports.reports;
+
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.manager.BaseReportManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jude on 6/3/2017.
+ */
+public class NumberOfTransfers extends BaseReportManager {
+
+	@Override
+	public String getUuid() {
+		return "b9c4a218-4840-11e7-a919-92ebcb67fe33";
+	}
+
+	@Override
+	public String getName() {
+		return "Number of Transfers (Java)";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Number of Transfers for a given location";
+	}
+
+	@Override
+	public List<Parameter> getParameters() {
+		List<Parameter> parameterArrayList = new ArrayList<Parameter>();
+		parameterArrayList.add(ReportingConstants.LOCATION_PARAMETER);
+		return parameterArrayList;
+	}
+
+	@Override
+	public ReportDefinition constructReportDefinition() {
+		ReportDefinition reportDef = new ReportDefinition();
+		reportDef.setUuid(getUuid());
+		reportDef.setName(getName());
+		reportDef.setDescription(getDescription());
+		reportDef.setParameters(getParameters());
+
+		SqlDataSetDefinition sqlDataDef = new SqlDataSetDefinition();
+		sqlDataDef.setName(getName());
+		sqlDataDef.addParameters(getParameters());
+		sqlDataDef.setSqlQuery(getSQLQuery());
+
+		reportDef.addDataSetDefinition("transferCount", Mapped.mapStraightThrough(sqlDataDef));
+
+
+		return reportDef;
+	}
+
+	@Override
+	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+		return null;
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
+
+	private String getSQLQuery(){
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("select 'Tranfers', count(*) as 'total' from encounter e ");
+		stringBuilder.append("where e.encounter_type=(select encounter_type_id from encounter_type where uuid='7b68d557-85ef-4fc8-b767-4fa4f5eb5c23') ");
+		stringBuilder.append("and e.location_id=:location ");
+		stringBuilder.append("and e.voided = 0 ");
+		stringBuilder.append("group by e.encounter_type ");
+
+		return stringBuilder.toString();
+	}
+}
