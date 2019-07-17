959ca2b3a01b7aa5004bfd854827cf26c9cd5241
HerbertYiga <herbertyiga24@gmail.com>
2019-07-17 15:33:16 +0300

    RA-923:Adding a Manage Extensions page

---
+++ omod/src/main/java/org/openmrs/module/referenceapplication/page/controller/ManageExtensionsPageController.java
@@ -0,0 +1,87 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.referenceapplication.page.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.openmrs.module.appframework.domain.Extension;
+import org.openmrs.module.appframework.service.AppFrameworkService;
+import org.openmrs.module.uicommons.UiCommonsConstants;
+import org.openmrs.module.uicommons.util.InfoErrorMessageUtil;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ManageExtensionsPageController {
+
+	public void get(PageModel model, @SpringBean("appFrameworkService") AppFrameworkService service) {
+		addModelAttributes(model, service);
+	}
+
+	public String post(PageModel model, @RequestParam("id") String id, @RequestParam("action") String action,
+			@SpringBean("appFrameworkService") AppFrameworkService service, HttpSession session, UiUtils ui) {
+		String successMsgCode = "referenceapplication.app.manageApps." + action + ".success";
+		String failMessageCode = "referenceapplication.app.manageApps." + action + ".fail";
+		try {
+			if ("enable".equals(action)) {
+				service.enableExtension(id);
+
+			} else if ("disable".equals(action)) {
+				service.disableExtension(id);
+			}
+
+			InfoErrorMessageUtil.flashInfoMessage(session, ui.message(successMsgCode, id));
+
+			return "redirect:referenceapplication/manageExtensions.page";
+		} catch (Exception e) {
+			session.setAttribute(UiCommonsConstants.SESSION_ATTRIBUTE_ERROR_MESSAGE, ui.message(failMessageCode, id));
+		}
+
+		addModelAttributes(model, service);
+
+		return null;
+	}
+
+	private void addModelAttributes(PageModel model, AppFrameworkService service) {
+
+		List<Extension> allExtensions = service.getAllExtensions();
+		List<Extension> enabledExtensions = service.getAllEnabledExtensions();
+		List<ExtensionModel> extensions = new ArrayList<ExtensionModel>();
+
+		for (Extension ex : allExtensions) {
+			extensions.add(new ExtensionModel(ex.getId(), enabledExtensions.contains(ex)));
+		}
+		model.addAttribute("extensions", extensions);
+	}
+
+	public class ExtensionModel {
+
+		private String id;
+
+		private boolean enabled;
+
+		public ExtensionModel(String id, boolean enabled) {
+			this.id = id;
+			this.enabled = enabled;
+
+		}
+
+	}
+}
