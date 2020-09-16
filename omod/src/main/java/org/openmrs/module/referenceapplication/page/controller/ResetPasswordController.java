15b12bd5a55d9a65999e916b740689aa3ac88f59
HerbertYiga <herbertyiga24@gmail.com>
2020-09-16 10:56:48 +0300

    RA-1753:Password Force change not working

--- omod/src/main/java/org/openmrs/module/referenceapplication/page/controller/ResetPasswordController.java
+++ omod/src/main/java/org/openmrs/module/referenceapplication/page/controller/ResetPasswordController.java
@@ -1,6 +1,10 @@
 package org.openmrs.module.referenceapplication.page.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -8,10 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.openmrs.PersonName;
 import org.openmrs.User;
+import org.openmrs.api.AdministrationService;
 import org.openmrs.api.UserService;
 import org.openmrs.api.context.Context;
+import org.openmrs.messagesource.MessageSourceService;
 import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.PrivilegeConstants;
 import org.openmrs.web.OptionsForm;
@@ -27,66 +34,85 @@ public class ResetPasswordController extends SimpleFormController{
 	
 
 
-	public ModelAndView onSubmit(HttpServletRequest request,HttpServletResponse  response,Object object,BindException errors)  throws Exception {
-		
-		
-		HttpSession httpSession=request.getSession();
-		
-		String view=getFormView();
-		
-		if(!errors.hasErrors()) {
-			
-			User loginUser=Context.getAuthenticatedUser();
-			UserService us=Context.getUserService();
-			User user=null;
-			
-			try {
-				
-				Context.addProxyPrivilege(PrivilegeConstants.GET_USERS);
-				user=us.getUser(loginUser.getId());
+	
+	
+	
+	
+	
+	/**
+	 * Called prior to form display. Allows for data to be put in the request to be used in the view
+	 * 
+	 * @see org.springframework.web.servlet.mvc.SimpleFormController#referenceData(javax.servlet.http.HttpServletRequest)
+	 */
+	
+	
+	 protected Map<String,Object>  referenceData(HttpServletRequest request)  throws  Exception{
+		 
+		 HttpSession httpSession=request.getSession();
+		 Map<String,Object> map=new HashMap<String,Object>();
+		 
+		 if(Context.isAuthenticated()) {
+			 
+			 Object resetPasswordAttribute=httpSession.getAttribute("resetPassword");
+			 if(resetPasswordAttribute == null) {
+				 resetPasswordAttribute= "";
 			 }
-			finally {
-				Context.removeProxyPrivilege(PrivilegeConstants.GET_USERS);
-			}
-			
-	  OptionsForm opts=(OptionsForm)object;
-	  
-	  Map<String ,String> properties=user.getUserProperties();
-	  
-	  
-	  properties.put(OpenmrsConstants.USER_PROPERTY_DEFAULT_LOCATION,opts.getDefaultLocation());
-	  
-	  
-	  Locale locale=WebUtil.normalizeLocale(opts.getDefaultLocale());
-	  if(locale !=null) {
-			properties.put(OpenmrsConstants.USER_PROPERTY_DEFAULT_LOCALE, locale.toString());
-			
-	  }
-	  
-	  properties.put(OpenmrsConstants.USER_PROPERTY_PROFICIENT_LOCALES,
-			    WebUtil.sanitizeLocales(opts.getProficientLocales()));
-	  properties.put(OpenmrsConstants.USER_PROPERTY_SHOW_RETIRED, opts.getShowRetiredMessage().toString());
-	  properties.put(OpenmrsConstants.USER_PROPERTY_SHOW_VERBOSE, opts.getVerbose().toString());
-	  
-	  properties.put(OpenmrsConstants.USER_PROPERTY_NOTIFICATION, opts.getNotification() == null ? "" : opts
-		        .getNotification().toString());
-		properties.put(OpenmrsConstants.USER_PROPERTY_NOTIFICATION_ADDRESS, opts.getNotificationAddress() == null ? ""
-		        : opts.getNotificationAddress().toString());
+			 else {
+			   httpSession.removeAttribute("resetPassword");
+			 
+			 }
+			 
+			 map.put("resetPassword",resetPasswordAttribute);
+				
+			//generate the password hint depending on the security GP settings
+			 
+			 List <String>hints=new ArrayList<String>(5);
+			 int minChar=1;
+			 AdministrationService as = Context.getAdministrationService();
 		
-			
-			
-			
-			
-		}
-		view = getSuccessView();
-		
-		
-		return new ModelAndView(new RedirectView(view));
+			 MessageSourceService mss=Context.getMessageSourceService();
+			 
+			 try {
+				 String minCharStr = as.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_MINIMUM_LENGTH);
+					if (StringUtils.isNotBlank(minCharStr)) {
+						minChar = Integer.valueOf(minCharStr);
+					}
+					if (minChar < 1) {
+						minChar = 1;
+					}
+			 }
+			 catch(NumberFormatException e) {
+				 
+			 }
 
-	}
-	
-	
-	
+				hints.add(mss.getMessage("options.login.password.minCharacterCount", new Object[] { minChar }, null));
+				addHint(hints, as.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_CANNOT_MATCH_USERNAME_OR_SYSTEMID),
+				    mss.getMessage("options.login.password.cannotMatchUsername"));
+				addHint(hints, as.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_REQUIRES_UPPER_AND_LOWER_CASE),
+				    mss.getMessage("options.login.password.containUpperCase"));
+				addHint(hints, as.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_REQUIRES_DIGIT),
+				    mss.getMessage("options.login.password.containNumber"));
+				addHint(hints, as.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_REQUIRES_NON_DIGIT),
+				    mss.getMessage("options.login.password.containNonNumber"));
+				
+				StringBuilder passwordHint = new StringBuilder("");
+				for (int i = 0; i < hints.size(); i++) {
+					if (i == 0) {
+						passwordHint.append(hints.get(i));
+					} else if (i < (hints.size() - 1)) {
+						passwordHint.append(", ").append(hints.get(i));
+					} else {
+						passwordHint.append(" and ").append(hints.get(i));
+					}
+				}
+				
+				map.put("passwordHint", passwordHint.toString());
+				
+
+		 }
+		 return map;
+		 
+	 }
 	
 	
 	/**
@@ -98,31 +124,37 @@ public class ResetPasswordController extends SimpleFormController{
 	
 	protected Object formBackingObject(HttpServletRequest request) throws ServletException {
 		
-		
 		OptionsForm opts=new OptionsForm();
 		if(Context.isAuthenticated()) {
 			User user=Context.getAuthenticatedUser();	
 			opts.setUsername(user.getUsername());
-			
 			PersonName personName;
 			if(user.getPersonName() !=null) {
-				personName=PersonName.newInstance(user.getPersonName());
-				personName.setPersonNameId(null);
+			  personName=PersonName.newInstance(user.getPersonName());
+			  personName.setPersonNameId(null);
 			}
 			else {
-				personName=new PersonName();
-				
-			}
+			  personName=new PersonName();
+			 }
 			
 			opts.setPersonName(personName);
-			
 		}
 		
-		
-		return opts;
-		
+		return opts;	
 	}
-	
+	/**
+	 * Utility method that check if a security property with boolean values is enabled and adds hint
+	 * message for it if it is not blank
+	 * 
+	 * @param hints
+	 * @param gpValue the value of the global property
+	 * @param message the localized message to add
+	 */
+	private void addHint(List<String> hints, String gpValue, String message) {
+		if (Boolean.valueOf(gpValue) && !StringUtils.isBlank(message)) {
+			hints.add(message);
+		}
+	}
 	
 }
  
